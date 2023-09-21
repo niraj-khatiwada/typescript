bef1636a4d376b305abf4c9f2391f66b8a6e1d4b
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-09-21 21:48:06 +0545

    basics

---
+++ 01/build/script.js
@@ -0,0 +1,6 @@
+"use strict";
+function test(a) {
+    console.log('Hello World!', a);
+}
+let s = 'Niraj';
+console.log(12 + s);
---
+++ 01/src/script.ts
@@ -0,0 +1,7 @@
+function test(a: number): void {
+  console.log('Hello World!', a)
+}
+
+let s: string = 'Niraj'
+
+console.log(12 / s)
